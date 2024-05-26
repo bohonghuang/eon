9a773adb3dce8a663159cfb8f4a238faf3581672
coco24 <1281299809@qq.com>
2024-05-26 17:39:12 +0800

    Add gamepad support

--- src/input.lisp
+++ src/input.lisp
@@ -25,6 +25,27 @@
             (:right . :right)))
   "An association list used to represent the mapping of controller buttons to RAYLIB:KEYBOARD-KEYs.")
 
+(defparameter *gamepad-button-mappings*
+  (mapcar (lambda (pair)
+            (cons (car pair) (foreign-enum-value 'raylib:gamepad-button (cdr pair))))
+          '((:a . :right-face-right)
+            (:b . :right-face-down)
+            (:x . :right-face-up)
+            (:y . :right-face-left)
+            (:start . :middle-right)
+            (:select . :middle-left)
+            (:l1 . :left-trigger-1)
+            (:r1 . :right-trigger-1)
+            (:l2 . :left-trigger-2)
+            (:r2 . :right-trigger-2)
+            (:l3 . :left-thumb)
+            (:r3 . :right-thumb)
+            (:up . :left-face-up)
+            (:down . :left-face-down)
+            (:left . :left-face-left)
+            (:right . :left-face-right)))
+  "An association list used to represent the mapping of controller buttons to RAYLIB:GAMEPAD-BUTTONs.")
+
 (defun controller-button-keyboard-key (controller-button)
   "Get the RAYLIB:KEYBOARD-KEY corresponding to CONTROLLER-BUTTON."
   (declare (type keyword controller-button))
@@ -36,21 +57,48 @@
   (declare (type fixnum keyboard-key))
   (rassoc-value *keyboard-key-mappings* keyboard-key))
 
+(defun controller-button-gamepad-button (controller-button)
+  "Get the gamepad button corresponding to CONTROLLER-BUTTON."
+  (or (assoc-value *gamepad-button-mappings* controller-button)
+      (error "Unknown controller button: ~S" controller-button)))
+
+(defun gamepad-button-controller-button (gamepad-button)
+  "Get the controller button corresponding to GAMEPAD-BUTTON."
+  (declare (type fixnum gamepad-button))
+  (rassoc-value *gamepad-button-mappings* gamepad-button))
+
+(defmacro do-available-gamepads ((var &optional (do :do)) &body body)
+  `(loop :for ,var :of-type non-negative-fixnum :from 0
+         :while (raylib:is-gamepad-available ,var)
+         ,do (progn . ,body)))
+
 (defun controller-button-pressed-p (button)
   "Return whether BUTTON has just been pressed."
-  (raylib:is-key-pressed (controller-button-keyboard-key button)))
+  (or (raylib:is-key-pressed (controller-button-keyboard-key button))
+      (let ((gamepad-button (controller-button-gamepad-button button)))
+        (do-available-gamepads (gamepad :thereis)
+          (raylib:is-gamepad-button-pressed gamepad gamepad-button)))))
 
 (defun controller-button-down-p (button)
   "Return whether BUTTON is being pressed."
-  (raylib:is-key-down (controller-button-keyboard-key button)))
+  (or (raylib:is-key-down (controller-button-keyboard-key button))
+      (let ((gamepad-button (controller-button-gamepad-button button)))
+        (do-available-gamepads (gamepad :thereis)
+          (raylib:is-gamepad-button-down gamepad gamepad-button)))))
 
 (defun controller-button-released-p (button)
   "Return whether BUTTON has just been released."
-  (raylib:is-key-released (controller-button-keyboard-key button)))
+  (or (raylib:is-key-released (controller-button-keyboard-key button))
+      (let ((gamepad-button (controller-button-gamepad-button button)))
+        (do-available-gamepads (gamepad :thereis)
+          (raylib:is-gamepad-button-released gamepad gamepad-button)))))
 
 (defun controller-button-up-p (button)
   "Return whether BUTTON is not being pressed."
-  (raylib:is-key-up (controller-button-keyboard-key button)))
+  (and (raylib:is-key-up (controller-button-keyboard-key button))
+       (let ((gamepad-button (controller-button-gamepad-button button)))
+         (do-available-gamepads (gamepad :always)
+           (raylib:is-gamepad-button-up gamepad gamepad-button)))))
 
 (defvar *previous-input-query-function* nil)
 
@@ -62,24 +110,26 @@
 (defparameter *controller-button-repeat-enabled-p* t)
 
 (defun pressed-controller-button ()
-  (let ((keycode (raylib:get-key-pressed)))
-    (if-let ((button (keyboard-key-controller-button keycode)))
-      (let ((repeat-time (+ (game-loop-time) 0.5d0)))
-        (when *controller-button-repeat-enabled-p*
-          (add-game-loop-hook
-           (lambda ()
-             (if (> (game-loop-time) repeat-time)
-                 (async
-                   (loop :while (and *controller-button-repeat-enabled-p* (controller-button-down-p button))
-                         :when (< (length *controller-button-queue*) +controller-button-queue-size-limit+)
-                           :do (nconcf *controller-button-queue* (list button))
-                         :do (await (promise-sleep (/ 5.0 60.0)))
-                         :finally (setf *controller-button-queue* nil)))
-                 (controller-button-up-p button)))
-           :before #'not))
-        (setf *previous-input-query-function* #'pressed-controller-button)
-        (return-from pressed-controller-button button))
-      (pop *controller-button-queue*))))
+  (if-let ((button (or (when-let ((keycode (raylib:get-key-pressed)))
+                         (keyboard-key-controller-button keycode))
+                       (when-let ((button (raylib:get-gamepad-button-pressed)))
+                         (gamepad-button-controller-button button)))))
+    (let ((repeat-time (+ (game-loop-time) 0.5d0)))
+      (when *controller-button-repeat-enabled-p*
+        (add-game-loop-hook
+         (lambda ()
+           (if (> (game-loop-time) repeat-time)
+               (async
+                 (loop :while (and *controller-button-repeat-enabled-p* (controller-button-down-p button))
+                       :when (< (length *controller-button-queue*) +controller-button-queue-size-limit+)
+                         :do (nconcf *controller-button-queue* (list button))
+                       :do (await (promise-sleep (/ 5.0 60.0)))
+                       :finally (setf *controller-button-queue* nil)))
+               (controller-button-up-p button)))
+         :before #'not))
+      (setf *previous-input-query-function* #'pressed-controller-button)
+      (return-from pressed-controller-button button))
+    (pop *controller-button-queue*)))
 
 (defun pressed-char ()
   "Get the recently pressed character, returning NIL if none was pressed."
