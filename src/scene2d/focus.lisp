7d4f73c395f7c1fffeaf9e442df70c05588bba65
coco24 <1281299809@qq.com>
2024-08-02 23:30:20 +0800

    Refactor `scene2d-focus-manager`

--- src/scene2d/focus.lisp
+++ src/scene2d/focus.lisp
@@ -47,9 +47,12 @@
       (scene2d-focusable-update-upper-focal-points focusable size-x size-y))
     (call-next-method)))
 
+(defparameter *scene2d-focus-manager-distance-ratio* 0.999)
+
 (defstruct scene2d-focus-manager
   "A structure that contains a list of SCENE2D-FOCUSABLEs."
-  (focusables nil :type list))
+  (focusables nil :type list)
+  (distance-ratio *scene2d-focus-manager-distance-ratio* :type single-float))
 
 (defun scene2d-focus-manager-focused (manager)
   "Get the currently focused SCENE2D-FOCUSABLE."
@@ -59,22 +62,23 @@
   "Make the MANAGER change the currently focused SCENE2D-FOCUSABLE based on the BUTTON (which can be :LEFT, :RIGHT, :UP, or :DOWN)."
   (with-accessors ((focusables scene2d-focus-manager-focusables))
       manager
-    (let ((focused (first focusables)))
-      (macrolet ((symmetric-impl (&body right-impl &aux
+    (let ((distance-ratio (scene2d-focus-manager-distance-ratio manager))
+          (focused (first focusables)))
+      (macrolet ((-+ (a b) `(- ,b ,a))
+                 (+- (a b) `(- ,a ,b))
+                 (symmetric-impl (&body right-impl &aux
                                                      (left-impl (copy-tree right-impl))
                                                      (up-impl (copy-tree right-impl))
                                                      (down-impl (copy-tree right-impl)))
                    (subst-swap left-impl
                      (:minimize :maximize)
                      (:right :left)
-                     (most-positive-single-float most-negative-single-float)
-                     (>= <=))
+                     (+- -+))
                    (subst-swap up-impl
                      (:minimize :maximize)
                      (:right :up)
                      (:left :down)
-                     (most-positive-single-float most-negative-single-float)
-                     (>= <=)
+                     (+- -+)
                      (raylib:vector2-x raylib:vector2-y))
                    (subst-swap down-impl
                      (:right :down)
@@ -89,43 +93,23 @@
          (let ((focused-point (scene2d-focusable-focal-point focused :right)))
            (multiple-value-bind (lower-bound upper-bound)
                (loop :for focusable :in focusables
-                     :for focal-point := (scene2d-focusable-focal-point focusable :left)
-                     :unless (eq focusable focused)
-                       :minimize (raylib:vector2-x focal-point) :into lower-bound :of-type single-float
-                       :and :maximize (raylib:vector2-x focal-point) :into upper-bound :of-type single-float
+                     :minimize (raylib:vector2-x (scene2d-focusable-focal-point focusable :left)) :into lower-bound :of-type single-float
+                     :maximize (raylib:vector2-x (scene2d-focusable-focal-point focusable :right)) :into upper-bound :of-type single-float
                      :finally (return (values lower-bound upper-bound)))
-             (let (candidates non-candidates)
-               (if (not (<= (raylib:vector2-x focused-point) upper-bound))
-                   (setf non-candidates (delete-if (lambda (focusable)
-                                                     (when (= (raylib:vector2-x (scene2d-focusable-focal-point focusable :left)) lower-bound)
-                                                       (push focusable candidates)))
-                                                   focusables))
-                   (flet ((candidate-valid-p (candidate)
-                            (and (not (eq candidate focused))
-                                 (>= (raylib:vector2-x (scene2d-focusable-focal-point candidate :left)) (raylib:vector2-x focused-point)))))
-                     (setf focusables (sort focusables #'<
-                                            :key (lambda (focusable)
-                                                   (raylib:%vector2-distance (& focused-point) (& (scene2d-focusable-focal-point focusable :left))))))
-                     (let ((valid-min-distance (raylib:%vector2-distance (& focused-point) (& (scene2d-focusable-focal-point (find-if #'candidate-valid-p focusables) :left)))))
-                       (setf non-candidates (delete-if (lambda (focusable)
-                                                         (when (candidate-valid-p focusable)
-                                                           (when (= (raylib:%vector2-distance (& (scene2d-focusable-focal-point focusable :left)) (& focused-point)) valid-min-distance)
-                                                             (push focusable candidates))))
-                                                       focusables)))))
-               (loop :with offset := (raylib:make-vector2)
-                     :with candidate-cons := candidates
-                     :and candidate-prev-cons := nil
-                     :for prev-cons := candidates :then cons
-                     :for cons :on (cdr candidates)
-                     :for point := (scene2d-focusable-focal-point (car cons) :left)
-                     :for candidate-point := (scene2d-focusable-focal-point (car candidate-cons) :left)
-                     :do (raylib:%vector2-subtract (& offset) (& point) (& focused-point))
-                     :when (< (abs (raylib:vector2-y offset))
-                              (progn
-                                (raylib:%vector2-subtract (& offset) (& candidate-point) (& focused-point))
-                                (abs (raylib:vector2-y offset))))
-                       :do (setf candidate-cons cons candidate-prev-cons prev-cons)
-                     :finally
-                        (when candidate-prev-cons
-                          (shiftf (cdr candidate-prev-cons) (cdr candidate-cons) candidates candidate-cons)))
-               (setf focusables (nconc candidates non-candidates))))))))))
+             (flet ((directional-distance (focusable &aux (focal-point (scene2d-focusable-focal-point focusable :left)))
+                      (let ((distance (+- (raylib:vector2-x focal-point) (raylib:vector2-x focused-point))))
+                        (if (minusp distance)
+                            (+ (+- (raylib:vector2-x focal-point) lower-bound)
+                               (+- upper-bound (raylib:vector2-x focused-point))
+                               1.0)
+                            distance)))
+                    (non-directional-distance (focusable &aux (focal-point (scene2d-focusable-focal-point focusable :left)))
+                      (abs (- (raylib:vector2-y focal-point) (raylib:vector2-y focused-point)))))
+               (flet ((distance (focusable)
+                        (if (and (= (raylib:vector2-x (scene2d-focusable-focal-point focused :left))
+                                    (raylib:vector2-x (scene2d-focusable-focal-point focusable :left)))
+                                 (= (raylib:vector2-x (scene2d-focusable-focal-point focused :right))
+                                    (raylib:vector2-x (scene2d-focusable-focal-point focusable :right))))
+                            most-positive-single-float
+                            (lerp distance-ratio (non-directional-distance focusable) (directional-distance focusable)))))
+                 (setf focusables (sort focusables #'< :key #'distance)))))))))))
